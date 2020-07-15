0a726fec0b7e3389cfeac85c67ac135a96ff0f4f
Diego Sonaglia <diego.sonaglia@gmail.com>
2020-07-15 19:00:41 +0200

    fiex riot stuff

--- src/riot-coap-saul/bin/esp8266-esp-12x/mtd/mtd-vfs.d
+++
@@ -1,5 +0,0 @@
-/home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/mtd/mtd-vfs.o: \
- /home/di3go/Documents/Coding/RIOT/drivers/mtd/mtd-vfs.c \
- /home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/riotbuild/riotbuild.h
-
-/home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/riotbuild/riotbuild.h:
--- src/riot-coap-saul/bin/esp8266-esp-12x/netopt/netopt.d
+++
@@ -1,8 +0,0 @@
-/home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/netopt/netopt.o: \
- /home/di3go/Documents/Coding/RIOT/sys/net/crosslayer/netopt/netopt.c \
- /home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/riotbuild/riotbuild.h \
- /home/di3go/Documents/Coding/RIOT/sys/include/net/netopt.h
-
-/home/di3go/Documents/Coding/riot-coap/bin/esp8266-esp-12x/riotbuild/riotbuild.h:
-
-/home/di3go/Documents/Coding/RIOT/sys/include/net/netopt.h:
