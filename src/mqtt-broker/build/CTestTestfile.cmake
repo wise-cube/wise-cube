1a199bffcd745d28213d66f5610e2dbf9c4a5032
Diego Sonaglia <diego.sonaglia@gmail.com>
2020-07-30 15:04:45 +0200

    remove build files 3rd attempt

---
+++ src/mqtt-broker/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/di3go/Documents/wise-cube-new/src/mqtt-broker/src
+# Build directory: /home/di3go/Documents/wise-cube-new/src/mqtt-broker/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("client")
+subdirs("src")
+subdirs("man")
