35a002e546ab371da6c3416012e1f639a0d4934c
Diego Sonaglia <diego.sonaglia@gmail.com>
2020-08-01 19:17:33 +0200

    fix to .gitignore and removed useless files

--- src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/mtd/mtd-vfs.d
+++
@@ -1,5 +0,0 @@
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/mtd/mtd-vfs.o: \
- /home/di3go/Documents/Coding/RIOT/drivers/mtd/mtd-vfs.c \
- /home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h
-
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h:
--- src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/shell_commands/sc_ps.d
+++
@@ -1,8 +0,0 @@
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/shell_commands/sc_ps.o: \
- /home/di3go/Documents/Coding/RIOT/sys/shell/commands/sc_ps.c \
- /home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h \
- /home/di3go/Documents/Coding/RIOT/sys/include/ps.h
-
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h:
-
-/home/di3go/Documents/Coding/RIOT/sys/include/ps.h:
