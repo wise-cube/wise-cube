bec22e8fb34f29c169893c85c4aa3e80f9a164f8
Diego Sonaglia <diego.sonaglia@gmail.com>
2020-08-01 19:17:33 +0200

    fix to .gitignore and removed useless files

--- src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/esp_wifi/esp_wifi_gnrc.d
+++
@@ -1,5 +0,0 @@
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/esp_wifi/esp_wifi_gnrc.o: \
- /home/di3go/Documents/Coding/RIOT/cpu/esp_common/esp-wifi/esp_wifi_gnrc.c \
- /home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h
-
-/home/di3go/Documents/wise-cube-new/src/cube-riot/paho-mqtt/bin/esp8266-esp-12x/riotbuild/riotbuild.h:
--- src/cube-riot/paho-mqtt/bin/native/cpu/tramp.d
+++
@@ -1,6 +0,0 @@
-/home/di3go/Documents/wise-cube.bak/src/cube-client-riot/paho-mqtt/bin/native/cpu/tramp.o: \
- /home/di3go/Documents/Coding/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /home/di3go/Documents/wise-cube.bak/src/cube-client-riot/paho-mqtt/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/di3go/Documents/wise-cube.bak/src/cube-client-riot/paho-mqtt/bin/native/riotbuild/riotbuild.h:
