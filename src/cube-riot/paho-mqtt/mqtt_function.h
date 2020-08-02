6d3c20646556715bc79aaaf85221c32aa63194ab
Giulai <giu.delci@gmail.com>
2020-08-02 22:16:13 +0200

    riot-mqtt

---
+++ src/cube-riot/paho-mqtt/mqtt_function.h
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "xtimer.h"
+#include "shell.h"
+#include "thread.h"
+#include "mutex.h"
+#include "paho_mqtt.h"
+#include "MQTTClient.h"
+
+#define BUF_SIZE                        1024
+#define MQTT_VERSION_v311               4       /* MQTT v3.1.1 version is 4 */
+#define COMMAND_TIMEOUT_MS              4000
+
+#ifndef DEFAULT_MQTT_CLIENT_ID
+#define DEFAULT_MQTT_CLIENT_ID          ""
+#endif
+
+#ifndef DEFAULT_MQTT_USER
+#define DEFAULT_MQTT_USER               ""
+#endif
+
+#ifndef DEFAULT_MQTT_PWD
+#define DEFAULT_MQTT_PWD                ""
+#endif
+
+/**
+ * @brief Default MQTT port
+ */
+#define DEFAULT_MQTT_PORT               1883
+
+/**
+ * @brief Keepalive timeout in seconds
+ */
+#define DEFAULT_KEEPALIVE_SEC           10
+
+#ifndef MAX_LEN_TOPIC
+#define MAX_LEN_TOPIC                   100
+#endif
+
+#ifndef MAX_TOPICS
+#define MAX_TOPICS                      4
+#endif
+
+#define IS_CLEAN_SESSION                1
+#define IS_RETAINED_MSG                 0
+
+static MQTTClient client;
+static Network network;
+static int topic_cnt = 0;
+static char _topic_to_subscribe[MAX_TOPICS][MAX_LEN_TOPIC];
+
+int disconnect(int argc, char **argv);
+
+int connect();
+
+int pub(char* topic, char* payload);
+
+int sub(char* topic);
+
+int _cmd_unsub(int argc, char **argv);
+
+void new_game(char* game_id);
+
+void new_answere(char* answere_id);
+
+void new_group_req();
+
+void resume_group_req(char* token);
