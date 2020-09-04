ed952949452ea7c9853c22df4c162204991f7252
Diego Sonaglia <diego.sonaglia@gmail.com>
2020-09-04 20:19:14 +0200

    better paho interface -w shell ( live \w rob)

--- src/riot-cube/paho/cube_functions.h
+++ src/riot-cube/paho/cube_functions.h
@@ -1,8 +1,10 @@
-//
-// Created by di3go on 2020-09-02.
-//
-
 #ifndef RIOT_CUBE_CUBE_FUNCTIONS_H
 #define RIOT_CUBE_CUBE_FUNCTIONS_H
 
+#include "paho.h"
+
+void cube_init(void);
+int cmd_pub_shake_event(int argc, char **argv);
+int cmd_pub_button_ok_event(int argc, char **argv);
+int cmd_pub_button_ko_event(int argc, char **argv);
 #endif //RIOT_CUBE_CUBE_FUNCTIONS_H
--- src/riot-cube/paho/utils.h
+++ src/riot-cube/paho/utils.h
@@ -1,23 +1,13 @@
 #ifndef RIOT_CUBE_UTILS_H
 #define RIOT_CUBE_UTILS_H
 
-#endif //RIOT_CUBE_UTILS_H
-#include "jsmn.h"
-
-#include <stdio.h>
-#include <string.h>
-//#include <stdbool.h>
-//#include "xtimer.h"
-//#include "shell.h"
-//#include "thread.h"
-//#include "mutex.h"
-//#include "paho_mqtt.h"
-//#include "MQTTClient.h"
-
 #define EXIT_SUCCESS 0
-
 #define EXIT_FAILURE 1
 
 
-int jsoneq(const char *json, jsmntok_t *tok, const char *s);
-int json_conv(char* msg);
+#include "globals.h"
+
+//int cmd_is_online(int argc, char **argv);
+int cmd_get_status(int argc, char **argv);
+
+#endif //RIOT_CUBE_UTILS_H
