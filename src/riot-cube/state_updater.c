f07c050c732b1ce5af063cd49cbec167a5c31133
di3go-sona <diego.sonaglia@gmail.com>
2020-10-28 02:11:42 +0100

    publishing current state to broker periodically

--- src/riot-cube/state_updater.c
+++ src/riot-cube/state_updater.c
@@ -4,7 +4,7 @@
 #include "utils.h"
 #include "mpu.h"
 #include "buttons.h"
-
+#define MSG_BUF_SIZE 256
 // #define STATE_UPDATER_FREQ_USEC _xtimer_ticks_from_usec(1000000)
 
 extern state_t current_state;
@@ -12,13 +12,21 @@ pid_t state_updater_pid;
 
 void set_state(state_t new_state){
     current_state = new_state;
-    state_update();
+    if (current_state == STATE_CONNECTED) {
+        state_update();
+    }
 }
 
 void state_update(void){
     thread_wakeup(state_updater_pid);
 }
+int pub_state(void){
+    char         msg_buf[MSG_BUF_SIZE];
+    const char  msg_template_buf[]    ="{\"msg_type\":\"%s\",\"state\":%d}";
 
+    snprintf(msg_buf, MSG_BUF_SIZE-1, msg_template_buf, "state_update", current_state);
+    return pub(PUB_TOPIC, msg_buf);
+}
 void state_update_internal(void){
     printf("Current state: %d\n", current_state);
      switch (current_state) {
@@ -28,8 +36,9 @@ void state_update_internal(void){
             case STATE_UNINITIALIZED:
                 led_set_color(RED);
                 break;
-            case STATE_INITIALIZED:
+            case STATE_DISCONNECTED:
                 led_set_color(YELLOW);
+                con();
                 break;
             case STATE_CONNECTED:
                 led_set_color(GREEN);
@@ -45,8 +54,10 @@ void state_update_internal(void){
 void* state_updater_thread_handler(void* data){
     // xtimer_ticks32_t last_wakeup = xtimer_now();
     while(true) {
-        state_update_internal();
         xtimer_sleep(3);
+        state_update_internal();
+        pub_state();
+        
         // xtimer_periodic_wakeup(&last_wakeup,STATE_UPDATER_FREQ_USEC);
     }
     
@@ -57,7 +68,7 @@ int state_updater_init(void) {
     char * status_updater_thread_stack = malloc(THREAD_STACKSIZE_MEDIUM);
     state_updater_pid = thread_create( status_updater_thread_stack,
             THREAD_STACKSIZE_MEDIUM  ,
-            11,
+            4,
             THREAD_CREATE_STACKTEST,
             state_updater_thread_handler ,
             NULL, "state_updater_thread");
@@ -65,22 +76,5 @@ int state_updater_init(void) {
     int err = state_updater_pid < 0;
     wlog_res("State updater init", err);
     return err;
-}
-
-
-void connect(void){
-    int err;
-    err = con();
-    if (!err){
-        current_state = STATE_CONNECTED;
-    } else {
-        current_state = STATE_INITIALIZED;
-    }
-
-}
-void disconnect(void){
-    // int err;
-    discon();
-    current_state = STATE_INITIALIZED;
 }
 
